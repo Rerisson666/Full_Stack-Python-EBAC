3da6249d517beb293e1fae71ea8d25d74faf861e
rerisson666 <rerisson.rodrigues@hotmail.com>
2024-03-09 08:58:42 -0300

    incluindo validação de segurança no forms

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,66 +1,123 @@
-
 function calcularMedia( notas ) {
-	
-	var soma = 0;
-	for( c = 0; c < notas.length; c++ ) {
-		soma += notas[c];
-	}
 
-	media = soma / notas.length;
+    let soma = 0;
+    for( c = 0; c < notas.length; c++) {
+        soma += notas[c];
+    }
 
-	return media;
+    media = soma / notas.length;
+
+    return media;
 
 }
 
-function aprovacao( notas ) {
-	
-	let media = calcularMedia( notas );
+let media; // escopo global
 
-	let condicao = media >= 8 ? "Aprovado" : "Reprovado";
+function aprovacao( notas ) {
+
+    let media = calcularMedia( notas ); // escopo da função
+
+    let condicao = media >= 8 ? "aprovado" : "reprovado";
 
     return 'Média: ' + media + ' - Resultado: ' + condicao;
 
 }
 
+
 // Função Recursivas
 
 function contagemRegressiva(numero){
 
-    console.log(numero);
-
+    console.log(numero);  
+    
     let proximoNumero = numero - 1;
-    if(proximoNumero >0)
+
+    if(proximoNumero > 0)
         contagemRegressiva(proximoNumero);
 
 }
 
-// contagemRegrssiva(50);
+// contagemRegressiva(50);
+/*
+    *Formulário de envio de dados para calculo da média
+*/
 
-document.addEventListener('submit', function( evento ){
+document.getElementById('formulario-01').addEventListener('submit', function( evento ){
 
     evento.preventDefault();
     evento.stopPropagation();
 
-    let formulario = document.getElementById('formulario-01');
+    if( this.getAttribute('class').match(/erro/) ) {
+        return false;
+    }
 
-    let dados = new FormData(formulario);
+    let dados = new FormData(this);
 
-    let objeto = {};
-    
     let notas = [];
 
-    for(let key of dados.key()){
-        objeto[key] = dados.get(key);
+    for(let key of dados.keys()) {
 
-        notas.push(parseInt(dados.get(key)));
+        let numero = dados.get(key).match(/\d*/) ? Number(dados.get(key)) : 0;
+        
+        if(!isNaN(numero)) {
+            notas.push(numero);
+        }
 
     }
 
     console.log(notas);
 
-    console.log(objeto);
+    texto = aprovacao(notas)
 
-    document.getElementById('resultado').innerHTML = aprovacao(notas);
+    document.getElementById('resultado').innerHTML = texto;
 
-})
+});
 
+
+function validaCampo(elemento){
+
+    elemento.addEventListener('focusout', function(event) {
+        event.preventDefault();
+
+        if(this.value == ''){
+            document.querySelectorAll('.mensagem').innerHTML = "verifique o preenchimento dos campos em vermelho"
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        } else {
+            document.querySelectorAll('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        }
+
+    });
+}
+
+function validaCampoNumerico(elemento){
+
+    elemento.addEventListener('focusout', function(event) {
+        event.preventDefault();
+
+        if(this.value != "" && this.value.match(/[0-9]*/) && this.value >= 0 && this.value <=10){
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');      
+        } else {
+            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em vermelho"
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;            
+        }
+
+    });
+}
+
+let camposObrigatorios = document.querySelectorAll('input.obrigatorio');
+let camposNumericos = document.querySelectorAll('input.numero');
+
+for (let emFoco of camposObrigatorios) {
+    validaCampo(emFoco)
+}
+for (let emFoco of camposNumericos) {
+    validaCampoNumerico(emFoco)
+}
